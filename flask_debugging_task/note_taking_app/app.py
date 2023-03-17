f3a61742e84203ba74cab55f809b238d7cc280a2
Alonge Daniel <alongedainel19@gmail.com>
2023-03-17 13:17:14 +0100

    debugging note app

---
+++ flask_debugging_task/note_taking_app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+notes = []
+@app.route('/', methods=["GET","POST"])
+def index():
+    note = request.args.get("note")
+    if note:
+        notes.append(note)
+    if note:
+        return render_template("home.html", notes=notes)
+    else:
+        return render_template("home.html")
+        
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
